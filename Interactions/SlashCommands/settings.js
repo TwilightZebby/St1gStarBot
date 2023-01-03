603565a6917797779bb416382b8195d62b81e951
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2023-01-03 14:03:45 +0000

    Bump DJS, Remove Localisation Files
    Only removing Localisation Files until I can figure out a better way to do localised strings!

--- Interactions/SlashCommands/settings.js
+++ Interactions/SlashCommands/settings.js
@@ -1,8 +1,6 @@
 const { ChatInputCommandInteraction, ChatInputApplicationCommandData, AutocompleteInteraction, ApplicationCommandType, PermissionFlagsBits, ApplicationCommandOptionType, ChannelType, EmbedBuilder, Colors, PermissionsBitField } = require("discord.js");
 const fs = require('fs');
-const { DiscordClient, Collections } = require("../../constants.js");
-const LocalizedErrors = require("../../JsonFiles/errorMessages.json");
-const LocalizedStrings = require("../../JsonFiles/stringMessages.json");
+const { DiscordClient } = require("../../constants.js");
 
 module.exports = {
     // Command's Name
